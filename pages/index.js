f158541e7bffb5d2ca7dd15aef12442d895a3e60
Red Williams <red.devcs@gmail.com>
2021-04-11 06:05:19 -0400

    Added home page, changed file structure
    This is the start of the migration from the old local repo. Each commit should add one component at a time, starting with the homepage. No more jsx, no more ugly file structures. Also testing if merging from prod will auto deploy on vercel

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
+// Next Modules
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+// Components
+import Home from "../src/components/Home"
+
+// CSS
+import styles from '../styles/page/Main.module.css'
+
+
+export default function Main() {
   return (
+
+    // <div className={styles.container}>
+    //   <Head>
+    //     <title>Create Next App</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
+
+    //   <main className={styles.main}>
+    //     <h1 className={styles.title}>
+    //       Welcome to <a href="https://nextjs.org">Next.js!</a>
+    //     </h1>
+
+    //     <p className={styles.description}>
+    //       Get started by editing{' '}
+    //       <code className={styles.code}>pages/index.js</code>
+    //     </p>
+
+    //     <div className={styles.grid}>
+    //       <a href="https://nextjs.org/docs" className={styles.card}>
+    //         <h3>Documentation &rarr;</h3>
+    //         <p>Find in-depth information about Next.js features and API.</p>
+    //       </a>
+
+    //       <a href="https://nextjs.org/learn" className={styles.card}>
+    //         <h3>Learn &rarr;</h3>
+    //         <p>Learn about Next.js in an interactive course with quizzes!</p>
+    //       </a>
+
+    //       <a
+    //         href="https://github.com/vercel/next.js/tree/master/examples"
+    //         className={styles.card}
+    //       >
+    //         <h3>Examples &rarr;</h3>
+    //         <p>Discover and deploy boilerplate example Next.js projects.</p>
+    //       </a>
+
+    //       <a
+    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //         className={styles.card}
+    //       >
+    //         <h3>Deploy &rarr;</h3>
+    //         <p>
+    //           Instantly deploy your Next.js site to a public URL with Vercel.
+    //         </p>
+    //       </a>
+    //     </div>
+    //   </main>
+
+    //   <footer className={styles.footer}>
+    //     <a
+    //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Powered by{' '}
+    //       <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+    //     </a>
+    //   </footer>
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+
+        <title>Red Williams - First-Year General Engineering Student</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Red Williams, Engineering/Computer Science student based in Blacksburg, Virginia" />
+        <meta name="keywords" content="Red Williams,Computer Science,Virginia Tech,Java,Python,HTML,JavaScript, web developer" />
+        <meta name="author" content="Red Williams" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Home />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
