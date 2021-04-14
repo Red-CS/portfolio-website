6d8a9b2690079b5be3ab610088e9f1fcb96c8fee
Red Williams <red.devcs@gmail.com>
2021-04-14 02:31:35 -0400

    fixed clean database call!
    Wow that worked. useEffect was the way to go. I can easily access the project data from the database directly within the component. Next commit should focus on cleaning the actual API so I can map out the format of the calls I'll take, as well as cleaning some of the code and catching errors

--- src/components/Third.js
+++ src/components/Third.js
@@ -1,7 +1,7 @@
 import styles from "../../styles/component/Third.module.css";
 import LeftFeaturedProject from "./LeftFeaturedProject";
 import RightFeaturedProject from "./RightFeaturedProject";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Start of Prisma integration
 import { PrismaClient } from "@prisma/client";
@@ -46,35 +46,17 @@ const featuredProjects = fetch("/api/featured-project", {
 
 // return response;
 
-// const showProject = () => {
-//     featuredProjects.then((a) => {
-//         console.log(a["projects"][0].project_name);
-//         return a["projects"][0].project_name;
-//     })
-// }
-
-// const showProject = async () => {
-//     const a = await featuredProjects;
-//     console.log(a["projects"][0].project_name);
-//     return a["projects"][0].project_name;
-// }
 
 export default function ThirdSection() {
-    const [projectName, setProjectName] = useState("Default Project");
-    const [projectDesc, setProjectDesc] = useState("Default Description");
-    const [projectTech1, setProjectTech1] = useState("Default Tech1");
-    const [projectTech2, setProjectTech2] = useState("Default Tech2");
-    const [projectGithub, setProjectGithub] = useState("www.defaultGithubLink.com");
-    const [projectProject, setProjectProject] = useState("www.defaultProjectLink.com");
-    featuredProjects.then(data => {
-        console.log(data)
-        setProjectName(data["projects"][0].project_name);
-        setProjectDesc(data["projects"][0].project_description);
-        setProjectTech1(data["projects"][0].tech_one);
-        setProjectTech2(data["projects"][0].tech_two);
-        setProjectGithub(data["projects"][0].github_link);
-        setProjectProject(data["projects"][0].project_link);
-    });
+    const [characterObject, setCharacterObject] = useState({})
+    useEffect(() => 
+        featuredProjects.then(data => {
+            console.log(data)
+            setCharacterObject(data)
+    }), [characterObject]);
+
+    console.log("Character Object:")
+    console.log(characterObject)
     return (
     <div className={styles["section", "third-section"]}>
         <div className={styles["my-work"]}>
@@ -107,14 +89,16 @@ export default function ThirdSection() {
                 imageSrc={"/img/2020-08-09.png"}
             />
             <LeftFeaturedProject 
-                title={projectName}
-                descriptionParagraph={projectDesc}
-                tech1={projectTech1}
-                tech2={projectTech2}
-                tech3={"No Data"}
+                title={characterObject["projects"][0].project_name} 
+                descriptionParagraph={`Lorem ipsum dolor sit amet.
+                    Ad placeat perferendis perspiciatis vitae.
+                    Consequuntur, neque? Suscipit, laboriosam itaque.
+                    Praesentium nemo temporibus magni sunt!
+                    Eligendi illum sed ipsam aperiam.`}
+                tech1={"Python"}
+                tech2={"Github API"}
+                tech3={"Batch"}
                 imageSrc={"/img/2020-08-09.png"}
-                githubLink={projectGithub}
-                projectLink={projectProject}
             />
             <br/>
             <br/>
