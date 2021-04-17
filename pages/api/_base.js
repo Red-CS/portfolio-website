ecca909b168721bd376ea5e49b5620aa69e2c8ba
Red Williams <red.devcs@gmail.com>
2021-04-17 09:04:01 -0400

    redefine working deployment

--- pages/api/_base.js
+++ pages/api/_base.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env.DATABASE_URL)
+console.log(process.env.SUPABASE_URL)
 const supabaseUrl = "https://kcnvttpaqvkyaqsiummu.supabase.co"
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxODYzOTIwMSwiZXhwIjoxOTM0MjE1MjAxfQ.LDccerMtdPIXqCd4afcshjakTWbU5jb53_vW5vqvcow'
 const supabase = createClient(supabaseUrl, supabaseKey)
