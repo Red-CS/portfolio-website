6a32c7c5db2503af1ba5fab4d57066056c9e05c5
Red Williams <red.devcs@gmail.com>
2021-04-18 03:09:51 -0400

    see envs in deploy 2

--- src/components/Third.js
+++ src/components/Third.js
@@ -9,7 +9,7 @@ import { useState, useEffect } from "react";
 // TODO: Change fetch url to https://redwilliams.dev/api/featured-project upon pushing
 console.log("Vercel URL: " + process.env.VERCEL_URL)
 console.log("NextPublic URL: " + process.env.NEXT_PUBLIC_VERCEL_URL)
-const featuredProjects = fetch(`http://localhost:3000/api/featured-project`, {
+const featuredProjects = fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/featured-project`, {
     method: "GET",
 })
     .then((response) => {
