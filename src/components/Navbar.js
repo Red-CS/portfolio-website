be6cf090d9cdfc6b42d88307463eeb9a2f9fb909
Red-CS <red.devcs@gmail.com>
2021-06-20 13:24:28 -0400

    main section mobile and ipad styles
    Finally, something I'm happy with

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Menu from "./Menu";
-import styles from "@styles/Navbar.module.css";
+import styles from "@styles/component/Navbar.module.css";
 
 const Navbar = () => {
   return (
